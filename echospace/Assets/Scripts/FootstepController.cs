374c322ca221d02140b900fdb14ff7b30dae7ce4
almacle <43427079+aidiin-MAC@users.noreply.github.com>
2026-03-03 13:02:25 -0600

    Implemented footstep system

--- echospace/Assets/Scripts/FootstepController.cs
+++ echospace/Assets/Scripts/FootstepController.cs
@@ -5,13 +5,20 @@ using System.Collections.Generic;
 public class FootstepController : MonoBehaviour
 {
     public int groundTerrain;
-    public Rigidbody myRigidbody;
+    public Transform myTransform;
+    public Dictionary<int, AudioClip> clipList;
     public List<AudioClip> placeholderClips;
-
+    public float oldX;
+    public float oldZ;
+    public float deltaX;
+    public float deltaZ;
+    public float deadZone;
+    
     /*public Lists mySoundBanks;
 
     public int listCount;*/
     public AudioClip chosenClip;
+    public AudioSource footstepSource;
     public float timer;
     public float cooldownTime;
 
@@ -19,17 +26,34 @@ public class FootstepController : MonoBehaviour
     void Start()
     {
         groundTerrain = 0;
-        timer = 0;
-        cooldownTime = .20F;
+        timer = cooldownTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer == 0)
+        deltaX = myTransform.position.x - oldX;
+        deltaZ = myTransform.position.z - oldZ;
+        oldX = myTransform.position.x;
+        oldZ = myTransform.position.z;
+        if (timer < 0)
         {
-
+            footstepSource.generator = chosenClip;
+            footstepSource.Play();
+            timer = cooldownTime;
+            Debug.Log("footstep");
+            chosenClip = placeholderClips[UnityEngine.Random.Range(0, 2)];
         }
+        else
+        {
+            if (math.abs(deltaX) > deadZone || math.abs(deltaZ) > deadZone)
+            {
+                timer -= Time.deltaTime;
+            }
+        }  
+            
+
+        
         /*if (math.abs(myRigidbody.linearVelocity.x) > 1 | math.abs(myRigidbody.linearVelocity.y) > 1)
         {
             switch (groundTerrain)
